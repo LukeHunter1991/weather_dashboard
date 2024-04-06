777b6efbc4c90adad5b467a77eef9ee3209ab3df
Luke Hunter <luke.hunter9@gmail.com>
2024-04-06 16:56:11 +1100

    Added search functions

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -4,6 +4,10 @@ let cityName = "";
 //const locationUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName},&limit=1&appid=${apiKey}`;
 const searchEl = document.getElementById("search-form");
 
+let searchList = JSON.parse(localStorage.getItem("searchList")) || [];
+const historyEl = document.getElementById('history');
+let searchSuccess = "";
+
 function geoAPI() {
     fetch(
         `http://api.openweathermap.org/geo/1.0/direct?q=${cityName},&limit=1&appid=${apiKey}`
@@ -12,19 +16,17 @@ function geoAPI() {
             return response.json();
         })
         .then(function (data) {
-            console.log(data.length);
             if (data.length == 0) {
+                searchSuccess = "fail";
                 alert("Location not found")
             } else {
                 weatherAPI(data[0].lat, data[0].lon);
+                searchSuccess = "success";
             }
         });
 }
 
 function weatherAPI(lat, lon) {
-
-    console.log(lat);
-    console.log(lon);
 
     fetch(
         `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`
@@ -36,7 +38,6 @@ function weatherAPI(lat, lon) {
             if (data.length == 0) {
                 alert("No weather information available");
             } else {
-                console.log(data.list);
                 UpdateDashboard(data.list);
             }
         });
@@ -51,9 +52,6 @@ function UpdateDashboard(dashboardData) {
     // Update dashboard weather values for current day
     for (let i = 0; i < 6; i++) {
 
-        console.log(`${count}`);
-        console.log(`${i}`);
-
         if (count > 39) {
             count = 39;
         }
@@ -64,7 +62,7 @@ function UpdateDashboard(dashboardData) {
         let cardTempEl = document.getElementById(`card-temp-${i}`);
         let cardWindEl = document.getElementById(`card-wind-${i}`);
         let cardHumidityEl = document.getElementById(`card-humidity-${i}`);
-        let today = dashboardData[count].dt_txt + " UTC"
+        let today = dashboardData[count].dt_txt.slice(0, 10);
 
         // Update card elements with each loop
         cardHeaderEl.textContent = today;
@@ -82,9 +80,44 @@ const handleSearch = function (e) {
 
     e.preventDefault();
     cityName = document.getElementById("search-city").value;
-    console.log(cityName);
     geoAPI();
+    if (searchSuccess === "fail") {
+        return
+    }
+    searchList.unshift(cityName)
+    if (searchList.length > 10) {
+        searchList.pop();
+    }
+    searchList = searchList.filter((value, index) => searchList.indexOf(value) === index);
+    localStorage.setItem('searchList', JSON.stringify(searchList));
+    displaySearch();
+    searchEl.reset();
 
 }
 
-searchEl.addEventListener('submit', handleSearch)
+function displaySearch() {
+
+    searchList = JSON.parse(localStorage.getItem('searchList'));
+
+    historyEl.innerHTML = ""
+
+    for (cities of searchList) {
+        const cityHistory = document.createElement('div');
+        cityHistory.setAttribute('class', 'cityHistory');
+        cityHistory.textContent = cities;
+        historyEl.append(cityHistory);
+    }
+}
+
+function historySearch(e) {
+    cityName = e.target.textContent;
+    geoAPI();
+}
+
+if (searchList.length > 0) {
+    displaySearch();
+}
+
+searchEl.addEventListener('submit', handleSearch);
+historyEl.addEventListener('click', historySearch);
+
